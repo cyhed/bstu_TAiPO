0a74f5c04e2846d0881cb693264862f3086b6ec9
cyhed <zolotovv453@gmail.com>
2024-10-23 11:32:01 +0300

    lab3 init

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Windows.Automation;
+namespace lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Process p = Process.Start("calc");
+
+            AutomationElement aeDesktop = AutomationElement.RootElement;
+
+            Condition frame = new PropertyCondition(AutomationElement.ClassNameProperty, "CalcFrame");
+            AutomationElement c_frame = aeDesktop.FindFirst(TreeScope.Children, frame);
+
+            //Click button
+           /* Condition butt = new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Button);
+            AutomationElement but_sev = c_frame.FindFirst(TreeScope.Children, butt);
+            InvokePattern btnSevenPattern = but_sev.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            btnSevenPattern.Invoke();*/
+
+
+            //Click button equal
+            Condition button_eq_condtn = new PropertyCondition(AutomationElement.NameProperty, "Равно");
+            AutomationElement button_eq_ae = c_frame.FindFirst(TreeScope.Children, button_eq_condtn);
+            Console.WriteLine("Searching equals..");
+
+            if(button_eq_ae != null)
+            {
+                InvokePattern button_eq_ptrn = button_eq_ae.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+                button_eq_ptrn.Invoke();
+
+                Console.WriteLine("OK");
+            }
+            else {
+                Console.WriteLine("err");
+            }
+            Console.ReadKey();
+        }
+    }
+}
